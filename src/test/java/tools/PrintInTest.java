1dfeb8d98a2657d647a80c1254e4043f71fcddcf
qq20004604 <20004604@qq.com>
2018-08-24 00:42:56 +0800

    add new，但是啥都没写

---
+++ src/test/java/tools/PrintInTest.java
@@ -0,0 +1,104 @@
+package tools;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+/**
+ * PrintIn Tester.
+ *
+ * @author Wang Dong
+ * @version 1.0
+ * @since 08/24/2018
+ */
+public class PrintInTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: setTips(String tips)
+     */
+    @Test
+    public void testSetTips() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: printIn()
+     */
+    @Test
+    public void testPrintIn() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: printInInt()
+     */
+    @Test
+    public void testPrintInInt() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: getUserInput()
+     */
+    @Test
+    public void testGetUserInput() throws Exception {
+        //TODO: Test goes here...
+    }
+
+    /**
+     * Method: isInputError()
+     */
+    @Test
+    public void testIsInputError() throws Exception {
+        //TODO: Test goes here...
+    }
+
+
+    /**
+     * Method: setUserInput()
+     */
+    @Test
+    public void testSetUserInput() throws Exception {
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = PrintIn.getClass().getMethod("setUserInput"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/
+    }
+
+    /**
+     * Method: setUserInputOnlyInt()
+     */
+    @Test
+    public void testSetUserInputOnlyInt() throws Exception {
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = PrintIn.getClass().getMethod("setUserInputOnlyInt"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/
+    }
+
+} 
